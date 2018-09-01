63ba086f26aebd1b521f9f19c68b3870141e7b8c
Chribtoper <xcstolfax@gmail.com>
2018-09-01 21:38:53 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,20 +10,6 @@ def my_select(collection)
  end
  else puts "Empty collection"
 end
+arr
+end
 
-def my_select(collection)
-   i = 0
-    new_arr = []
-      if collection.length > 0
-      while i < collection.length
-       if yield(collection[i])
-       new_arr << collection[i]
-     end
-       i+=1
-     end
-   else
-    puts "Empty collection"
-   end
-   new_arr
-    end
-# call your method here!
